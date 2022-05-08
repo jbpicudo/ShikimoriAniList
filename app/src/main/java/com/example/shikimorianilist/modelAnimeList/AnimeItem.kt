9e227b3da1b1baa77987a2307ddf022226353302
jbpicudo <kanaye12@gmail.com>
2022-05-08 22:47:34 +0300

    model for animeList and animeRandom

---
+++ app/src/main/java/com/example/shikimorianilist/modelAnimeList/AnimeItem.kt
@@ -0,0 +1,31 @@
+package com.example.shikimorianilist.modelAnimeList
+
+
+import com.google.gson.annotations.SerializedName
+
+data class AnimeItem(
+    @SerializedName("aired_on")
+    val airedOn: String,
+    @SerializedName("episodes")
+    val episodes: Int,
+    @SerializedName("episodes_aired")
+    val episodesAired: Int,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("image")
+    val image: Image,
+    @SerializedName("kind")
+    val kind: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("released_on")
+    val releasedOn: String,
+    @SerializedName("russian")
+    val russian: String,
+    @SerializedName("score")
+    val score: String,
+    @SerializedName("status")
+    val status: String,
+    @SerializedName("url")
+    val url: String
+)
---
+++ app/src/main/java/com/example/shikimorianilist/modelAnimeList/Image.kt
@@ -0,0 +1,15 @@
+package com.example.shikimorianilist.modelAnimeList
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Image(
+    @SerializedName("original")
+    val original: String,
+    @SerializedName("preview")
+    val preview: String,
+    @SerializedName("x48")
+    val x48: String,
+    @SerializedName("x96")
+    val x96: String
+)
