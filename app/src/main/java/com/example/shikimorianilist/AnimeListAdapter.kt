ba876dd08ff9e2fc3db1296ae467f7226209682f
jbpicudo <kanaye12@gmail.com>
2022-05-09 06:11:16 +0300

    added base AnimeListAdapter.kt

---
+++ app/src/main/java/com/example/shikimorianilist/AnimeListAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.shikimorianilist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.shikimorianilist.databinding.AnimeItemBinding
+import com.example.shikimorianilist.modelAnimeList.AnimeItem
+import com.example.shikimorianilist.networkRetrofitAPI.RetrofitClient
+
+class AnimeListAdapter(private val animeItemList: MutableList<AnimeItem>) :
+    RecyclerView.Adapter<AnimeListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = AnimeItemBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val animeItem = animeItemList[position]
+        holder.bind(animeItem)
+    }
+
+    override fun getItemCount(): Int {
+        return animeItemList.size
+    }
+
+    fun addAnimeItem(animeItemList: MutableList<AnimeItem>) {
+        this.animeItemList.addAll(animeItemList)
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(animeItemLayoutBinding: AnimeItemBinding) :
+        RecyclerView.ViewHolder(animeItemLayoutBinding.root) {
+
+        private val binding = animeItemLayoutBinding
+
+        fun bind(animeItem: AnimeItem) {
+            binding.tvTitle.text = animeItem.russian
+
+            Glide.with(itemView.context)
+                .load(RetrofitClient.getBaseURL() + animeItem.image.preview)
+                .into(binding.ivAnimeCover)
+        }
+    }
+}
