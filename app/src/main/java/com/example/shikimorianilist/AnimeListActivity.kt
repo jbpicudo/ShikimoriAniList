9dc6714fd369b4c92e80fa1e185180461ecc1739
jbpicudo <kanaye12@gmail.com>
2022-05-10 05:12:09 +0300

    added base pagination

--- app/src/main/java/com/example/shikimorianilist/AnimeListActivity.kt
+++ app/src/main/java/com/example/shikimorianilist/AnimeListActivity.kt
@@ -3,6 +3,7 @@ package com.example.shikimorianilist
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shikimorianilist.databinding.ActivityAnimeListBinding
 import com.example.shikimorianilist.modelAnimeList.AnimeItem
 import com.example.shikimorianilist.networkRetrofitAPI.RetrofitClient
@@ -14,8 +15,10 @@ import retrofit2.Response
 class AnimeListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAnimeListBinding
-
     private lateinit var animeListAdapter: AnimeListAdapter
+
+    var page: Int = 1
+    var isLoading: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,11 +34,26 @@ class AnimeListActivity : AppCompatActivity() {
         binding.rvAnimeList.layoutManager = LinearLayoutManager(this)
         binding.rvAnimeList.adapter = animeListAdapter
 
+        binding.rvAnimeList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+
+                if (!recyclerView.canScrollVertically(1) && newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    if (!isLoading) {
+                        page++
+                        loadAnimeList()
+                    }
+                }
+            }
+        })
+
         loadAnimeList()
     }
 
     fun loadAnimeList() {
-        RetrofitClient.shikimoriAPI.getAnimeList(50, "ranked", 1, "")
+        isLoading = true
+
+        RetrofitClient.shikimoriAPI.getAnimeList(20, "ranked", page, "")
             .enqueue(object : Callback<List<AnimeItem>?> {
                 override fun onResponse(
                     call: Call<List<AnimeItem>?>,
@@ -45,10 +63,14 @@ class AnimeListActivity : AppCompatActivity() {
                     list.addAll(response.body()!!)
 
                     animeListAdapter.addAnimeItem(list)
+
+                    binding.tvPages.text = "Страница $page"
+                    isLoading = false
                 }
 
                 override fun onFailure(call: Call<List<AnimeItem>?>, t: Throwable) {
                     binding.tvPages.text = t.toString()
+                    isLoading = false
                 }
             })
     }
